2dcf9039a829159b58769405fc7c392722bbd673
Ritik Chourasiya <ritvikchoure65+git@gmail.com>
2023-06-13 17:11:05 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {process.env.REACT_APP_API_KEY}
         </p>
         <a
           className="App-link"
