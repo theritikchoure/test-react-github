3b0a93070f5b3ba1ce0ee47f9e86b48ee3f3b900
Ritik Chourasiya <ritvikchoure65+git@gmail.com>
2023-06-13 17:15:41 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to rhlle.
           {process.env.REACT_APP_API_KEY}
         </p>
         <a
